448a15c0ce523d8fc6f1c0c132a3ab9fde102d44
collinsodhiambo <collinsodhiambo8290@gmail.com>
2025-11-01 08:05:53 +0300

    Added code to test the move logic in the main.cpp file

--- main.cpp
+++ main.cpp
@@ -6,7 +6,19 @@ int main() {
 
 	Board myBoard; // Call the constructor and sets up the pieces
 
+	std::cout << "Initial Board:" << std::endl;
 	myBoard.print();
+
+	// Make the e2 to e4 pawn move.
+	// e2 is (row 6, col 4) --> White piece
+	// e4 is row 4, col 4 --> Row 0 is the eighth rank, row 7 first and so on
+	Move whitePawnMove = {6, 4, 4, 4}; // {from_row, from_col, to_row, to_col}
+
+	// Make the move
+	myBoard.makeMove(whitePawnMove);
+
+	std::cout << "Board after 1. e4:" << std::endl;
+	myBoard.print(); // Print the board to see the move
 
 	return 0;
 }
