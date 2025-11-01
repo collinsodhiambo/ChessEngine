4e395cb9a19a1aefb9ccaee2f801a013666c2852
collinsodhiambo <collinsodhiambo8290@gmail.com>
2025-11-01 07:53:35 +0300

    Added a data structure to the header file to hold move data

--- Board.h
+++ Board.h
@@ -8,6 +8,14 @@
 enum Piece { EMPTY = 0, W_PAWN = 1, W_KNIGHT = 2, W_BISHOP = 3, W_ROOK = 4, W_QUEEN = 5, W_KING = 6,
 	B_PAWN = -1, B_KNIGHT = -2, B_BISHOP = -3, B_ROOK = -4, B_QUEEN = -5, B_KING = -6 };
 
+// A simple structure to hold move data
+struct Move {
+	int from_row;
+	int from_col;
+	int to_row;
+	int to_col;
+};
+
 class Board {
 	public:
 		// Constructor:
