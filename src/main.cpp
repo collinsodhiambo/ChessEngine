ff0b82e9545789323effcac08871b52c76cd3069
collinsodhiambo <collinsodhiambo8290@gmail.com>
2025-11-02 14:13:40 +0300

    Implemented tests for castling

--- src/main.cpp
+++ src/main.cpp
@@ -21,58 +21,51 @@ int main() {
 
 	Board myBoard; // Call the constructor and sets up the pieces
 
-	std::cout << "--- Testing En Passant ---" << std::endl;
+	std::cout << "--- Testing Castling ---" << std::endl;
 
-	// e4
 	myBoard.makeMove(Move{6, 4, 4, 4});
+        myBoard.makeMove(Move{1, 4, 3, 4});
+        myBoard.makeMove(Move{7, 6, 5, 5});
+        myBoard.makeMove(Move{0, 1, 2, 2});
+        myBoard.makeMove(Move{7, 5, 4, 2});
 
-	// A nothing move a6
-	myBoard.makeMove(Move{1, 0, 2, 0});
-
-	// e5
-	myBoard.makeMove(Move{4, 4, 3, 4});
-
-	// f5 (Black pawn moves 2 squares next to white's e5 pawn)
-	myBoard.makeMove(Move{1, 5, 3, 5});
-
-	// Board state after Black's f5
 	std::cout << "Board state:" << std::endl;
 	myBoard.print();
 
 
-	// -- TEST
-	// It's White's turn. The e5 pawn at (3, 4) should have a legal en passant
-	// capture to f6 at (2, 5)
+	// -- TEST 1: Find White's Castling Move (after Black Moves)
+	myBoard.makeMove(Move{0, 5, 3, 2});
+
 	std::cout << "--- Getting White's legal moves... ---" << std::endl;
 	std::vector<Move> legalMoves = myBoard.getLegalMoves();
 	printMoves(legalMoves);
 
 	// Find the en passant move
-	bool found_en_passant = false;
-	Move en_passant_move = Move{3, 4, 2, 5}; // e5 to f6
+	bool found_castle_move = false;
+	Move en_passant_move = Move{7, 4, 7, 6}; // e1 to g1
 
 	for (const Move& move: legalMoves) {
 		if (move.from_row == en_passant_move.from_row &&
 			move.to_row == en_passant_move.to_row &&
 			move.from_col == en_passant_move.from_col &&
 			move.to_col == en_passant_move.to_col) {
-				found_en_passant = true;
+				found_castle_move = true;
 				break;
 				}
 		}
 
-	if (found_en_passant) {
-		std::cout << "\n TEST 1 PASSED: Engine correctly found en passant move!" << std::endl;
+	if (found_castle_move) {
+		std::cout << "\n TEST 1 PASSED: Engine correctly found Kingside castling!" << std::endl;
 	} else {
-		std::cout << "\n TEST 1 FAILED: Engine did NOT find en passant move." << std::endl;
+		std::cout << "\n TEST 1 FAILED: Engine did NOT find castling move." << std::endl;
 	}
 
 	// TEST 2: Make the move
-	std::cout << "\n Making En Passant Move..." << std::endl;
+	std::cout << "\n Making Castling Move..." << std::endl;
 	myBoard.makeMove(en_passant_move);
 	myBoard.print();
-	std::cout << "Check that the White pawn is on f6 (2, 5) and" << std::endl;
-	std::cout << "the Black pawn on f5 (3, 5) has been removed." << std::endl;
+	std::cout << "Check that the White King is on g1 (7, 6) and" << std::endl;
+	std::cout << "the White rook is on f1 (7, 5) has been removed." << std::endl;
 
 	return 0;
 }
