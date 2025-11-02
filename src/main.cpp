cdf3b548b6b6da5171998e0a4d93d110d8a0f85c
collinsodhiambo <collinsodhiambo8290@gmail.com>
2025-11-02 13:17:08 +0300

    Test for En Passant moves

--- src/main.cpp
+++ src/main.cpp
@@ -21,42 +21,58 @@ int main() {
 
 	Board myBoard; // Call the constructor and sets up the pieces
 
-	std::cout << "--- Testing for Fool's Mate ---" << std::endl;
+	std::cout << "--- Testing En Passant ---" << std::endl;
 
-	// f3
-	Move m1 = {6, 5, 5, 5}; // (White)
-	myBoard.makeMove(m1);
+	// e4
+	myBoard.makeMove(Move{6, 4, 4, 4});
+
+	// A nothing move a6
+	myBoard.makeMove(Move{1, 0, 2, 0});
 
 	// e5
-	Move m2 = {1, 4, 3, 4}; // (Black)
-	myBoard.makeMove(m2);
+	myBoard.makeMove(Move{4, 4, 3, 4});
 
-	// g4
-	Move m3 = {6, 6, 4, 6}; // White
-	myBoard.makeMove(m3);
+	// f5 (Black pawn moves 2 squares next to white's e5 pawn)
+	myBoard.makeMove(Move{1, 5, 3, 5});
 
-	// Qh4# (checkmate)
-	Move m4 = {0, 3, 4, 7}; // Black
-	myBoard.makeMove(m4);
-
-	// The Board is now checkmate
+	// Board state after Black's f5
+	std::cout << "Board state:" << std::endl;
 	myBoard.print();
 
 
 	// -- TEST
-	// It's White's turn, and White is in checkmate
-	// getLegalMoves() should return an empty list
+	// It's White's turn. The e5 pawn at (3, 4) should have a legal en passant
+	// capture to f6 at (2, 5)
 	std::cout << "--- Getting White's legal moves... ---" << std::endl;
 	std::vector<Move> legalMoves = myBoard.getLegalMoves();
 	printMoves(legalMoves);
 
-	std::cout << "Total moves found: " << legalMoves.size() << std::endl; // Should be 16 + 4 for knight
+	// Find the en passant move
+	bool found_en_passant = false;
+	Move en_passant_move = Move{3, 4, 2, 5}; // e5 to f6
 
-	if (legalMoves.size() == 0) {
-		std::cout << "TEST PASSED: Engine correctly found 0 moves (Checkmate!)" << std::endl;
+	for (const Move& move: legalMoves) {
+		if (move.from_row == en_passant_move.from_row &&
+			move.to_row == en_passant_move.to_row &&
+			move.from_col == en_passant_move.from_col &&
+			move.to_col == en_passant_move.to_col) {
+				found_en_passant = true;
+				break;
+				}
+		}
+
+	if (found_en_passant) {
+		std::cout << "\n TEST 1 PASSED: Engine correctly found en passant move!" << std::endl;
+	} else {
+		std::cout << "\n TEST 1 FAILED: Engine did NOT find en passant move." << std::endl;
 	}
-	else {
-		std::cout << "TEST FAILED: Engine found " << legalMoves.size() << " moves." << std::endl;
-	}
+
+	// TEST 2: Make the move
+	std::cout << "\n Making En Passant Move..." << std::endl;
+	myBoard.makeMove(en_passant_move);
+	myBoard.print();
+	std::cout << "Check that the White pawn is on f6 (2, 5) and" << std::endl;
+	std::cout << "the Black pawn on f5 (3, 5) has been removed." << std::endl;
+
 	return 0;
 }
